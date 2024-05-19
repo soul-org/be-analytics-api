2bbebaa3adda17c35a85fc1e115c82484f54f7e7
Dheeraj (DJ) Agrawal <agrawal.dheeraj.7@gmail.com>
2024-05-19 20:19:12 +1000

    Adding validator address

--- src/staking/staking.service.ts
+++ src/staking/staking.service.ts
@@ -11,15 +11,15 @@ import type { Staker } from "./staking.interface";
 const stakers: Staker[] = [
 	{
 		token: "MATIC",
-		address: "0x01",
+		address: "0xAFfb23A344B7ebdf4Ea6B5ec27ECC00D12fecd77",
 		commission: 5,
 		apy: 10,
 	},
 	{
 		token: "AVAX",
-		address: "0x01",
-		commission: 5,
-		apy: 10,
+		address: "NodeID-4KXitMCoE9p2BHA6VzXtaTxLoEjNDo2Pt",
+		commission: 20,
+		apy: 6.6,
 	},
 ];
 
